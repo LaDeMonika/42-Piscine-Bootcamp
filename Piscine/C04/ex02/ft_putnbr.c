ab2e3a139ec5fe127a1eb3a882e766b6a186a1a3
LaDeMonika <128793184+LaDeMonika@users.noreply.github.com>
2023-07-27 20:28:45 +0200

    Update ft_putnbr.c

--- Piscine/C04/ex02/ft_putnbr.c
+++ Piscine/C04/ex02/ft_putnbr.c
@@ -74,3 +74,45 @@ int main()
     ft_putchar('\n');
 }
 */
+//Lili
+/*
+#include <unistd.h>
+#include <limits.h>
+
+char	*ft_putnbr(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(nb * (-1));
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		digit = nb + '0';
+		write(1, &digit, 1);
+	}
+	return (number);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nbr;
+
+	nbr = -2147483648;
+	ft_putnbr(nbr);
+}	
+*/
